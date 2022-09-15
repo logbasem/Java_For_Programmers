4220943d208c0577006e3114756f8d4b94e59b1f
logbasem <logan.murphy@plowtech.net>
2022-09-15 11:06:39 -0500

    First project
    Basic introduction to Java

---
+++ Project_1.java
@@ -0,0 +1,17 @@
+/** This program prints out fun facts about CS.
+ * @author Logan Murphy
+ * @version 1.0
+ * I copied this code from the Project 1 Handout, but it's OK because
+ * Sudhi Gopal told me to.
+ */
+public class Project_1 {
+	
+	//This is the main program. The instructions here are what
+	//is done when the program is run.
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to CS 1321, Fall 2022");
+		System.out.println("This is Project 1 and it is just a sample program");
+	}
+	
+}
